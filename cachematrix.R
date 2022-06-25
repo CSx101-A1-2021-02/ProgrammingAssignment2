476bb521c7c8eef40c7294c67cdc1a535ec1fe1b
Duc Thinh Nguyen <ducthinhbiotech@gmail.com>
2022-06-25 15:56:04 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(solve) m <<- solve
+        getmean <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             get = getsolve)
 }
+
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting inversed matrix of ")
+                return(m)
+        }
+        data <- x$get()
+        sol <- solve(data, ...)
+        x$setsolve(sol)
+        sol
 }
+
+
